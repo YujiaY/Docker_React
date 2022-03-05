7e5a5345d308278a95dec874e06a3eaa4b043322
Jack Yuan <jack.yujia.yuan@gmail.com>
2022-03-05 22:37:22 +1000

    Changed Instance types to: t3a.nano

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hell o World! Hei hei hei. Changed Header.
+          Hell o World! Hei hei hei. Changed Instance types to: t3a.nano.
         </p>
         <a
           className="App-link"
